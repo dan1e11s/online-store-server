f9e13ac5d96cc2b02f3382d852eada2821e8603c
Daniel <danielmuratov2003@gmail.com>
2024-06-24 21:31:37 +0600

    edit migrations

--- src/migrations/1719231090857-CreateProductAndCartTables.ts
+++ src/migrations/1719231090857-CreateProductAndCartTables.ts
@@ -10,8 +10,8 @@ class CreateProductAndCartTables1719231090857 {
         images TEXT[],
         price DECIMAL NOT NULL,
         category VARCHAR NOT NULL,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
+        createdAt TIMESTAMP DEFAULT NOW(),
+        updatedAt TIMESTAMP DEFAULT NOW()
       );
     `);
 
@@ -20,8 +20,6 @@ class CreateProductAndCartTables1719231090857 {
         id SERIAL PRIMARY KEY,
         product_id INT NOT NULL,
         quantity INT NOT NULL,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW(),
         CONSTRAINT fk_product
           FOREIGN KEY(product_id) 
           REFERENCES product(id)
